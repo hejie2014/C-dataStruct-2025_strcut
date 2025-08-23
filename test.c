b5a2952a86cded76b9f361365f2198a0268eb5fe
hejie2014 <2014119hu@gmail.com>
2025-08-23 17:41:12 +0800

    双向链表测试

--- test.c
+++ test.c
@@ -1,11 +1,65 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Node
+{
+    int data;
+    struct Node *prev;
+    struct Node *next;
+
+} Node;
+
+// ...existing code...
+void Insert(Node **head, int data)
+{
+    Node *temp = malloc(sizeof *temp);
+    if (temp == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        // 若需要，可在此释放已有节点再退出
+        exit(EXIT_FAILURE);
+    }
+    temp->data = data;
+    temp->prev = NULL;
+    temp->next = NULL;
+    if (*head == NULL)
+    {
+        *head = temp;
+        return;
+    }
+    // 需要遍历到最后一个节点
+    Node *temp2 = *head;
+    while (temp2->next != NULL)
+    {
+        temp2 = temp2->next;
+    }
+    temp2->next = temp;
+    temp->prev = temp2;
+}
+
+void freeList(Node **head)
+{
+    Node *cur = *head;
+    while (cur != NULL)
+    {
+        Node *next = cur->next;
+        free(cur);
+        cur = next;
+    }
+    *head = NULL;
+}
+// ...existing code...
 int main()
 {
-    int a = 10;
-    int b = 20;
-    int sum = a + b;
+    Node *head = NULL;
+    Insert(&head, 3);
+    Insert(&head, 4);
+    Insert(&head, 5);
+    Insert(&head, 6);
+    Insert(&head, 7);
+    Print(head);
 
-    printf("Hello, World!\n");
-    printf("Sum: %d\n", sum);
+    freeList(&head); // 释放内存
+
+    return 0;
 }
